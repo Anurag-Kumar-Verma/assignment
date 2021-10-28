3646f3c0de61537870a2bc6562a82c5e161ca9ff
Anurag-Kumar-Verma <anuragkumar26696@gmail.com>
2021-10-28 09:40:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App-container">
+      <table>
+        <thead>
+          <tr>
+            <td>Home</td>
+            <td>Address</td>
+            <td>Phone Number</td>
+            <td>Email</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Jeny Chan</td>
+            <td>3 waterfoot road</td>
+            <td>333-666-1546</td>
+            <td>jeny@gmail.com</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
